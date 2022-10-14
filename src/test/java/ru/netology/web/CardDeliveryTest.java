9e3ed43bf72e4535f2d2d5b1f5e3504d6b10c5ef
SvetlanaVasilek <vasilek378@gmail.com>
2022-10-15 00:22:03 +0300

    initial commit

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,46 +1,41 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static java.time.Duration.ofMillis;
 import static java.time.Duration.ofSeconds;
 import static java.time.format.DateTimeFormatter.ofPattern;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static com.codeborne.selenide.Condition.exactText;
 
 
 public class CardDeliveryTest {
 
-    @Test
-    void shouldOkWhenAllFillIn() {
-        open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Казань");
-        SelenideElement dateField = $("[placeholder='Дата встречи']"); // вводим элемент поля с датой
-        dateField.click();
-        String date = LocalDate.now().plusDays(3).format(ofPattern("dd.MM.yyyy")); // переменная с логикой даты по условиям
-        dateField.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE)); // очищаем поле с датой
-        dateField.setValue(date); // устанавливаем дату
-        $("[data-test-id='name'] input").setValue("Иванов Иван");
-        $("[data-test-id='phone'] input").setValue("+79250001122");
-        $("[data-test-id='agreement'] span").click();
-        $("[type='button']").click();
-        $(withText("Забронировать")).click();
-        $("[data-test-id=notification] .notification__content").shouldBe(visible, ofSeconds(15))
-                .shouldHave(exactText("Встреча успешно забронирована на " + date));
+        @Test
+        void shouldOkWhenAllFillIn() {
 
+            open("http://localhost:9999");
+            $("[data-test-id='city'] input").setValue("Казань");
+            SelenideElement dateField = $("[placeholder='Дата встречи']"); // вводим элемент поля с датой
+            dateField.click();
+            String date = LocalDate.now().plusDays(3).format(ofPattern("dd.MM.yyyy")); // переменная с логикой даты по условиям
+            dateField.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE)); // очищаем поле с датой
+            dateField.setValue(date); // устанавливаем дату
+            $("[data-test-id='name'] input").setValue("Иванов Иван");
+            $("[data-test-id='phone'] input").setValue("+79250001122");
+            $("[data-test-id='agreement'] span").click();
+            $("[type='button']").click();
+            $(withText("Забронировать")).click();
+            $("[data-test-id=notification] .notification__content").shouldBe(visible, ofSeconds(15))
+                    .shouldHave(exactText("Встреча успешно забронирована на " + date));
+
+        }
     }
-}
